7f592c25245a01a9c44498e722de82a49c585e47
ChIntU@1297 <akhilguthi94@gmail.com>
2025-08-26 18:10:01 +0530

    java commit 1

---
+++ src/javamodulespractices/HelloWorld.java
@@ -0,0 +1,19 @@
+package javamodulespractices;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+
+		String  A = "Hello World";
+		
+		System.out.println(A);
+		System.out.println(A.length());
+		System.out.println(A.toLowerCase());
+		String B = A.replace("Hello", "");
+		System.out.println(B);
+		
+	}
+
+	
+}
